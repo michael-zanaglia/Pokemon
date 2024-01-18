744cb8718684f460b96528fac57744b6fce287f2
Alessio <alessio.garofalo@laplateforme.io>
2024-01-18 02:54:10 +0100

    g

--- graphique/code map/switch.py
+++
@@ -1,9 +0,0 @@
-import pygame
-
-
-class Switch:
-    def __init__(self, type: str, name: str, hitbox: pygame.Rect, port: int):
-        self.hitbox = hitbox
-        self.port = port
-        self.name = name
-        self.type = type
--- graphique/code map/tool.py
+++
@@ -1,6 +0,0 @@
-import pygame
-
-class Tool:
-    @staticmethod
-    def split_image(spritesheet: pygame.Surface, x:int, y:int, width:int, height:int):
-        return spritesheet.subsurface(pygame.Rect(x, y, width, height))
